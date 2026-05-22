df7f346ef7efd8eade14f3c3a17b5b411bcbbe8b
Hanpla <hanpla25@gmail.com>
2026-05-22 12:51:59 +0900

    test: trivago 테스트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css';
 import type { Metadata } from 'next';
 import { ReactNode } from 'react';
+
+import '@/styles/globals.css';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
