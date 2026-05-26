6fcd7b01506e97ed051e244fad2af9a5a9ee3873
Sung_yuseung <usngsung0830@naver.com>
2026-05-26 15:18:37 +0900

    feat: Gnb 및 로그인/회원가입 버튼에 페이지 이동 링크 추가

--- src/components/common/Gnb/AuthButtons.tsx
+++ src/components/common/Gnb/AuthButtons.tsx
@@ -1,8 +1,18 @@
+import Link from 'next/link';
+
 export default function AuthButtons() {
   return (
-    <ul>
-      <li>로그인</li>
-      <li>회원가입</li>
+    <ul className="flex items-center justify-between gap-6.25">
+      <li>
+        <Link href="/login" className="hover:underline">
+          로그인
+        </Link>
+      </li>
+      <li>
+        <Link href="/signup" className="hover:underline">
+          회원가입
+        </Link>
+      </li>
     </ul>
   );
 }
--- src/components/common/Gnb/Gnb.tsx
+++ src/components/common/Gnb/Gnb.tsx
@@ -1,9 +1,16 @@
+import Link from 'next/link';
+
 import AuthButtons from './AuthButtons';
 
 export default function Gnb() {
   return (
-    <div>
-      <AuthButtons />
+    <div className="flex h-17.5 items-center justify-center border-b border-[#DDDDDD] p-6 max-md:p-5">
+      <div className="flex w-full max-w-300 items-center justify-between">
+        <Link href="/" className="cursor-pointer">
+          <span>Logo</span>
+        </Link>
+        <AuthButtons />
+      </div>
     </div>
   );
 }
