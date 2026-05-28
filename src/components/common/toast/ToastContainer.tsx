fa19680111a8a22cfc8a6343e3189bda55ff616f
gnath <rudqor1618@naver.com>
2026-05-28 17:09:30 +0900

    fix: toast 없을 때 컨테이너 미렌더링 및 레이아웃 마운트

--- src/components/common/toast/ToastContainer.tsx
+++ src/components/common/toast/ToastContainer.tsx
@@ -7,6 +7,8 @@ import Toast from './Toast';
 export default function ToastContainer() {
   const toasts = useToastStore((state) => state.toasts);
 
+  if (toasts.length === 0) return null;
+
   return (
     <div className="fixed bottom-6 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center gap-2">
       {toasts.map((toast) => (
