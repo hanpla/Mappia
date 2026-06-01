b2f2d41afb63972fe7f9832f3df549b2390a19b7
Hanpla <hanpla25@gmail.com>
2026-06-01 10:45:49 +0900

    refactor: 공통 프로필 타이틀로 분리

--- src/app/(main)/profile/dashboard/page.tsx
+++ src/app/(main)/profile/dashboard/page.tsx
@@ -2,12 +2,15 @@ import { Suspense } from 'react';
 
 import ReservationsContent from '@/components/profile-dashboard/ReservationsContent';
 import ReservationsSkeleton from '@/components/profile-dashboard/ReservationsSkeleton';
-import Title from '@/components/profile-dashboard/Title';
+import Title from '@/components/profile-title/Title';
 
 export default function DashboardPage() {
   return (
-    <div className="mt-6 md:mt-8">
-      <Title />
+    <div>
+      <Title title="예약 현황" />
+      <p className="text-gray-500">
+        내 체험에 예약된 내역들을 한 눈에 확인할 수 있습니다.
+      </p>
 
       <Suspense fallback={<ReservationsSkeleton />}>
         <ReservationsContent />
