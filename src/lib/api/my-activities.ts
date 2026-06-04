bd5207a4f2a3abc494bc47f5446caebc6dbd9408
Hanpla <hanpla25@gmail.com>
2026-06-04 15:10:50 +0900

    refactor: 기존인스턴스 대신 임시 api 작성

--- src/lib/api/my-activities.ts
+++ src/lib/api/my-activities.ts
@@ -1,24 +1,62 @@
-import type { MyActivitiesContent } from '@/types/my-activities';
+'use server';
+import { cookies } from 'next/headers';
 
-import { privateInstance } from './instance';
+import axios from 'axios';
+
+import type { MyActivitiesContent } from '@/types/my-activities';
 
 export interface GetMyActivitiesParams {
   cursorId?: number | null;
   size?: number;
 }
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const getAccessToken = async () => {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('accessToken')?.value;
+
+  if (!token) {
+    throw new Error('로그인이 필요합니다.');
+  }
+
+  return token;
+};
+
 export const getMyActivities = async (
   params?: GetMyActivitiesParams,
 ): Promise<MyActivitiesContent> => {
-  const response = await privateInstance.get<MyActivitiesContent>(
-    '/my-activities',
-    {
+  try {
+    const accessToken = await getAccessToken();
+
+    const res = await axios.get(`${BASE_URL}/my-activities`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
       params,
-    },
-  );
-  return response.data;
+    });
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+  return {
+    cursorId: null,
+    totalCount: 0,
+    activities: [],
+  };
 };
 
-export const deleteMyActivity = async (activityId: number): Promise<void> => {
-  await privateInstance.delete(`/my-activities/${activityId}`);
+export const deleteMyActivity = async (activityId: number) => {
+  try {
+    const accessToken = await getAccessToken();
+    await axios.delete(`${BASE_URL}/my-activities/${activityId}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
