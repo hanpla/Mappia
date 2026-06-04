89678d58ce8ae195c51b4829d9ce6589ede47c5c
gnath <rudqor1618@naver.com>
2026-06-05 04:24:29 +0900

    refactor: 내 체험 관리 헤더 컴포넌트 분리
    - 페이지를 서버 컴포넌트로 유지하기 위해 헤더(ManagesHeader) 분리
    - 등록 페이지 이동 버튼을 헤더 컴포넌트로 이동

--- src/app/(main)/profile/manages/page.tsx
+++ src/app/(main)/profile/manages/page.tsx
@@ -1,21 +1,13 @@
 import { Suspense } from 'react';
 
-import Button from '@/components/common/button/Button';
 import ListComponent from '@/components/profile-manages/ListComponent';
 import ListSkeleton from '@/components/profile-manages/ListSkeleton';
-import Title from '@/components/profile-title/Title';
+import ManagesHeader from '@/components/profile-manages/ManagesHeader';
 
 export default function ManagesPage() {
   return (
     <div>
-      <Title
-        title="내 체험 관리"
-        action={
-          <Button className="bg-brown-2A2 w-30 rounded-md p-0">
-            체험 등록하기
-          </Button>
-        }
-      />
+      <ManagesHeader />
       <Suspense fallback={<ListSkeleton />}>
         <ListComponent />
       </Suspense>
