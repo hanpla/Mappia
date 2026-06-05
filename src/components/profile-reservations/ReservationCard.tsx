e2c669355c1430e745fb5deb2020f8e85c718fee
jiwonll <166104093+jiwonll@users.noreply.github.com>
2026-06-05 14:18:41 +0900

    refactor: 가격 위 여백 조정

--- src/components/profile-reservations/ReservationCard.tsx
+++ src/components/profile-reservations/ReservationCard.tsx
@@ -169,7 +169,7 @@ export default function ReservationCard({
           </p>
         </div>
 
-        <div className="mt-3 flex flex-wrap items-center justify-between gap-1 md:mt-5 lg:mt-7">
+        <div className="mt-auto flex flex-wrap items-center justify-between gap-1 pt-2 lg:mt-6">
           <span className="text-black-1B1 textxl-bold flex-shrink-0 text-sm md:text-base">
             ₩{totalPrice.toLocaleString()}
           </span>
