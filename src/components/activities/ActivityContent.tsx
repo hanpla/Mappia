6f08f3e1fd86c67fb3a16b38ff180a357fc969a0
kujihye <gji135@naver.com>
2026-06-11 13:42:31 +0900

    feat: ActivityContent 컴포넌트 작업

--- src/components/activities/ActivityContent.tsx
+++ src/components/activities/ActivityContent.tsx
@@ -1,32 +1,42 @@
+import { ActivityDetailContent } from '@/types/activities';
+
 import ActivityDescription from '@/components/activities/ActivityDescription';
 import ActivityHeader from '@/components/activities/ActivityHeader';
-import Map from '@/components/activities/Map';
+import ActivityMap from '@/components/activities/ActivityMap';
 import MobileReservation from '@/components/activities/MobileReservation';
 import PhotoGallery from '@/components/activities/PhotoGallery';
 import Reservation from '@/components/activities/Reservation';
 import Review from '@/components/activities/Review';
 
-export default function ActivityContent() {
+interface ActivityContentProps {
+  activity: ActivityDetailContent;
+  currentPage: number;
+}
+
+export default function ActivityContent({
+  activity,
+  currentPage,
+}: ActivityContentProps) {
   return (
-    <div className="pt-[24px] lg:pt-[72px]">
+    <div className="pt-6 lg:pt-18">
       <div className="flex flex-col lg:grid lg:grid-cols-8 lg:gap-10">
-        <div className="space-y-5 md:space-y-[30px] lg:col-span-5 lg:space-y-10">
-          <PhotoGallery />
+        <div className="space-y-5 md:space-y-7.5 lg:col-span-5 lg:space-y-10">
+          <PhotoGallery activity={activity} />
           <div className="block lg:hidden">
-            <ActivityHeader />
+            <ActivityHeader activity={activity} />
           </div>
-          <ActivityDescription />
-          <Map />
-          <Review />
+          <ActivityDescription description={activity.description} />
+          <ActivityMap address={activity.address} />
+          <Review activityId={activity.id} currentPage={currentPage} />
         </div>
 
         <div className="hidden space-y-16 lg:col-span-3 lg:block">
-          <ActivityHeader />
-          <Reservation />
+          <ActivityHeader activity={activity} />
+          <Reservation activity={activity} />
         </div>
       </div>
 
-      <MobileReservation />
+      <MobileReservation activity={activity} />
     </div>
   );
 }
