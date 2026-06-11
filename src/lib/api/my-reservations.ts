d1cb212080c8786aeab3555023da60a3c11c9731
jiwonll <166104093+jiwonll@users.noreply.github.com>
2026-06-11 11:45:40 +0900

    refactor/ 에러처리 삭제

--- src/lib/api/my-reservations.ts
+++ src/lib/api/my-reservations.ts
@@ -19,20 +19,8 @@ export interface GetMyReservationsParams {
 export const getMyReservations = async (
   params?: GetMyReservationsParams,
 ): Promise<MyReservationsContent> => {
-  try {
-    const res = await privateInstance.get('/my-reservations', { params });
-    return res.data;
-  } catch (error: unknown) {
-    let errorMessage = '내 예약 조회에 실패했습니다.';
-
-    if (axios.isAxiosError(error)) {
-      errorMessage = error.response?.data?.message || errorMessage;
-    } else if (error instanceof Error) {
-      errorMessage = error.message;
-    }
-
-    throw new Error(errorMessage);
-  }
+  const res = await privateInstance.get('/my-reservations', { params });
+  return res.data;
 };
 
 // 리뷰 등록
@@ -40,48 +28,21 @@ export const createReview = async (
   reservationId: number,
   body: CreateReviewRequest,
 ): Promise<CreateReviewContent> => {
-  try {
-    const res = await privateInstance.post(
-      `/my-reservations/${reservationId}/reviews`,
-      body,
-    );
-    return res.data;
-  } catch (error: unknown) {
-    let errorMessage = '리뷰 등록에 실패했습니다.';
-
-    if (axios.isAxiosError(error)) {
-      errorMessage = error.response?.data?.message || errorMessage;
-    } else if (error instanceof Error) {
-      errorMessage = error.message;
-    }
-
-    throw new Error(errorMessage);
-  }
+  const res = await privateInstance.post(
+    `/my-reservations/${reservationId}/reviews`,
+    body,
+  );
+  return res.data;
 };
 
 // 예약 취소
 export const cancelReservation = async (
   reservationId: number,
 ): Promise<unknown> => {
-  try {
-    const res = await privateInstance.patch(
-      `/my-reservations/${reservationId}`,
-      {
-        status: 'canceled',
-      },
-    );
-    return res.data;
-  } catch (error: unknown) {
-    let errorMessage = '예약 취소에 실패했습니다.';
+  const res = await privateInstance.post(
+    `/my-reservations/${reservationId}/cancel`,
+  );
 
-    if (axios.isAxiosError(error)) {
-      errorMessage = error.response?.data?.message || errorMessage;
-    } else if (error instanceof Error) {
-      errorMessage = error.message;
-    }
-
-    throw new Error(errorMessage);
-  }
+  return res.data;
 };
-
 // 내 예약 수정, 취소 추후 구현 예정
